caac89b79d5d53655a50e9f9733c0600b1703a19
Bo Ye <boy.e.computer.1982@outlook.com>
2018-07-19 18:04:27 +1200

    Add User page

--- src/Accounts.js
+++ src/Accounts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Association from './Association'
+import User from './User'
 
 class Accounts extends Component{
   render(){
@@ -14,14 +15,19 @@ class Accounts extends Component{
                     <Link to='/Association'>Administrator</Link>
                 </div>
                 <div>
-                    <Link to='/Vote' style={linkStyle}>User1</Link>
-                    <Link to='/Vote' style={linkStyle}>User2</Link>
-                    <Link to='/Vote' style={linkStyle}>User3</Link>
-                    <Link to='/Vote' style={linkStyle}>User4</Link>
-                    <Link to='/Vote' style={linkStyle}>User5</Link>
+                    <Link to='/user/1' style={linkStyle}>User1</Link>
+                    <Link to='/user/2' style={linkStyle}>User2</Link>
+                    <Link to='/user/3' style={linkStyle}>User3</Link>
+                    <Link to='/user/4' style={linkStyle}>User4</Link>
+                    <Link to='/user/5' style={linkStyle}>User5</Link>
+                    <Link to='/user/6' style={linkStyle}>User6</Link>
+                    <Link to='/user/7' style={linkStyle}>User7</Link>
+                    <Link to='/user/8' style={linkStyle}>User8</Link>
+                    <Link to='/user/9' style={linkStyle}>User9</Link>
                 </div>
                 <hr />
                 <Route path="/Association" component={Association} />
+                <Route path="/user/:userId" component={User} />
             </div>
         </Router>
         );
--- src/User.js
+++ src/User.js
@@ -3,33 +3,27 @@ import Web3 from "web3";
 import AdvancedTokenContract from '../build/contracts/AdvancedToken.json'
 import AssociationContract from '../build/contracts/Association.json'
 
-class Association extends Component{
+class User extends Component{
   constructor(props) {
     super(props);
+    this.userId = this.props.match.params.userId;
     this.state = {
       balance: 0,
       minimumSharesToPassAVote: 0,
-      minimumSharesToPassAVoteInput: '',
-      minutesForDebate: 0,
-      minutesForDebateInput: ''
+      minutesForDebate: 0
     };
-
-    this.handleMinimumSharesToPassAVoteInputChange = this.handleMinimumSharesToPassAVoteInputChange.bind(this);
-    this.handleMinutesForDebateInputChange = this.handleMinutesForDebateInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
     var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
     this.web3 = new Web3(provider);
-    this.administrator = this.web3.eth.accounts[0];
+    var user = this.web3.eth.accounts[this.userId];
     const contract = require('truffle-contract');
     //AdvancedToken contract
     const AdvancedToken = contract(AdvancedTokenContract);
     AdvancedToken.setProvider(this.web3.currentProvider);
     AdvancedToken.deployed().then(instance => {
-        this.sharesAddress = instance.address;
-        return instance.balanceOf(this.administrator);
+        return instance.balanceOf(user);
     }).then(balance => {
       this.setState({
         balance: balance.toNumber()
@@ -56,25 +50,6 @@ class Association extends Component{
       });
   }
 
-   handleMinimumSharesToPassAVoteInputChange(event) {
-      this.setState({minimumSharesToPassAVoteInput: event.target.value});
-    }
-
-   handleMinutesForDebateInputChange(event) {
-      this.setState({minutesForDebateInput: event.target.value});
-   }
-
-  handleSubmit(event) {
-    this.association.changeVotingRules(this.sharesAddress, this.state.minimumSharesToPassAVoteInput, this.state.minutesForDebateInput, {from: this.administrator}).then(() => {
-       this.setVotingRulesState();
-       this.setState({
-               minimumSharesToPassAVoteInput: '',
-               minutesForDebateInput: ''
-       });
-    });
-    event.preventDefault();  //to prevent submit event from navigating to another page
-  }
-
   render(){
     var divSubmit = {
       width: 500,
@@ -85,10 +60,10 @@ class Association extends Component{
         };
     return (
       <div >
-        <div>Administrator&#39;s current token balance: {this.state.balance}</div>
+        <div>User{this.userId}&#39;s current token balance: {this.state.balance}</div>
         <div>
             <fieldset>
-                <legend>Association:</legend>
+                <legend>User:</legend>
                 <div>
                     <h5>Voting Rules</h5>
                     <p>Minimum shares to pass a vote: {this.state.minimumSharesToPassAVote}, minutes for debate: {this.state.minutesForDebate}</p>
@@ -97,22 +72,6 @@ class Association extends Component{
                     <h6>Proposals</h6>
                 </div>
             </fieldset>
-        </div>
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <h3>Change Voting Rules</h3>
-                <div>
-                    <label>Minimum shares to pass a vote: </label>
-                    <input type="text" value={this.state.minimumSharesToPassAVoteInput} onChange={this.handleMinimumSharesToPassAVoteInputChange}/>
-                </div>
-                <div>
-                    <label>Minutes for debate: </label>
-                    <input type="text" value={this.state.minutesForDebateInput} onChange={this.handleMinutesForDebateInputChange}/>
-                </div>
-                <div style={divSubmit}>
-                    <input type="submit" style={inputSubmit}  value="Submit"/>
-                </div>
-            </form>
         </div>
         <div>
             <h3>Create a proposal</h3>
@@ -126,4 +85,4 @@ class Association extends Component{
   }
 }
 
-export default Association;
+export default User;
